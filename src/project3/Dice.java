c7136221be5d5ad36db429775c390964fdacdbea
BlaneBrownie <73854500+BlaneBrownie@users.noreply.github.com>
2020-11-06 09:20:00 -0600

    Update Dice.java
    Revised to dice class so now when creating a dice object you chose how many dice you want to roll

--- src/project3/Dice.java
+++ src/project3/Dice.java
@@ -9,35 +9,42 @@ import java.util.Random;
 
 /**
  *
- * @author blane
+ * @author Blane Brown
  */
 public class Dice {
     
     //private data members
-    //the face of the dice thats showing
-    private final int face;
+    //the rolls of the dice stored in an array
+    private final int[] rolls;
 
     //constructor
     //Dice value will be a number 0 - 5, where 0 = arrow, 1 = dynamite, 
     //2 = one shot, 3 = two shot, 4 = gatling gun, 5 = beer, this can be seen 
-    //in toString 
-    Dice(){
+    //in toString, n is the number of dice to be rolled 
+    Dice(int n){
         Random rnd = new Random();
-        this.face = rnd.nextInt(6);
-    }
+        this.rolls = new int[n];
+        for(int i = 0; i < n; i++){
+            rolls[i] = rnd.nextInt(6);
+        }
+    } 
     
     //accessor
-    //gets the dices current showing face
-    public int getFace(){
-        return this.face;
+    //gets the array of dice roll values
+    public int[] getRolls(){
+        return this.rolls;
     }
     
     //other methods
-    //toString makes the face value a word
+    //toString makes the array of rolls a easier to read list
     @Override
     public String toString(){
         String[] faces = {"Arrow", "Dynamite", "One Shot", "Two Shot", "Gatling Gun", "Beer"};
-        String output = faces[this.face];
-        return output;
+        String output = "";
+        for(int i = 0; i < rolls.length; i++){
+            output = output + faces[rolls[i]] + ", ";
+        }
+        String finalOutput = output.substring(0, output.lastIndexOf(", "));
+        return finalOutput;
     }
 }
