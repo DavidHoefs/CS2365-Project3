1515a57908242f6b2956b2365ff7202b6c9bb8d4
ZHUNT64 <61096461+ZHUNT64@users.noreply.github.com>
2020-11-29 14:41:40 -0600

    Added Duel Dice data

--- src/myproject3/UndeadAliveDice.java
+++ src/myproject3/UndeadAliveDice.java
@@ -4,10 +4,41 @@
  */
 package myproject3;
 
+import java.util.Random;
+
 /**
  *
- * @author zhunt
+ * @author zhunt, hoeffs
  */
-public class UndeadAliveDice {
+public class UndeadAliveDice extends MyDice {
+    
+    private int roll;
+    
+    public UndeadAliveDice(String type){
+        Random rnd = new Random();
+        this.roll = rnd.nextInt(6);
+    }
+    
+    @Override
+    public int getRoll(){
+        return this.roll;
+    }
+    
+     //returns the value of a dice roll as a string,
+    @Override
+    public String getRollString(){
+        
+        String[] rollTypes = {"Arrow","Dynamite","Whiskey","Gatling Gun","Duel","Duel" };
+        return rollTypes[roll];
+    }
+    
+    
+    
+    //returns the value of a 5 dice roll as a string
+    @Override
+    public String getRollsString(int n){
+        String[] rollTypes = {"Arrow","Dynamite","Whiskey","Gatling Gun","Duel","Duel" };
+        return rollTypes[n];
+    } 
     
 }
